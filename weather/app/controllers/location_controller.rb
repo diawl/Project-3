f856c5111c8bb26ca3e03ac8fa83531545954368
cristianmonterom <cristian.monterom@outlook.com>
2015-05-22 16:27:44 +1000

    Added css for tables and changed location field type

--- weather/app/controllers/location_controller.rb
+++ weather/app/controllers/location_controller.rb
@@ -1,2 +1,8 @@
 class LocationController < ApplicationController
+  def index
+    @locations = Location.all
+  end
+  def show
+    @locations = Location.all
+  end
 end
--- weather/db/migrate/20150514041020_create_locations.rb
+++ weather/db/migrate/20150514041020_create_locations.rb
@@ -2,11 +2,16 @@ class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
       t.belongs_to :postcode, index: true
+<<<<<<< Updated upstream
       t.string :loc_id
+=======
+      t.string :loc_id, unique: true
+>>>>>>> Stashed changes
       t.float :lat
       t.float :lon
 
       t.timestamps null: false
     end
+
   end
 end
