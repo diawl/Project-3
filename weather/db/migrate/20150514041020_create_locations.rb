89801e21be7d496857d638bec1b7d16fd79c7443
cristianmonterom <cristian.monterom@outlook.com>
2015-05-22 16:29:31 +1000

    Updated location field

--- weather/db/migrate/20150514041020_create_locations.rb
+++ weather/db/migrate/20150514041020_create_locations.rb
@@ -2,11 +2,7 @@ class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
       t.belongs_to :postcode, index: true
-<<<<<<< Updated upstream
-      t.string :loc_id
-=======
       t.string :loc_id, unique: true
->>>>>>> Stashed changes
       t.float :lat
       t.float :lon
 
